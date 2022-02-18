0c26be39d6e7471ceef7a84b3e885066a1228cf6
Jeferson Ferreira <jefersonferreira@Mac-mini-de-Jeferson.local>
2022-02-18 18:52:02 -0300

    Commit inicial

---
+++ PrimeiroApp/PrimeiroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAppApp.swift
+//  PrimeiroApp
+//
+//  Created by Jeferson Ferreira on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
